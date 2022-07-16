3baae068f39dd20abe7ea1b1f5be2ce56f0dbf73
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-07-16 16:34:21 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,14 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+function EyesOnMe() {
+    return(
+        <button onFocus={focus} onBlur={blur}>Eyes on me</button>
+    )
+}
+function blur(){
+    console.log('Hey! Eyes on me!')
+}
+function focus() {
+    console.log('Good!')
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,13 @@
 // Code Keypad Component Here
+
+
+import React from 'react'
+function Keypad() {
+    function change(){
+        console.log('Entering password...')
+    }
+    return(
+        <input onChange={change} type="password"/>
+    )
+}
+export default Keypad
